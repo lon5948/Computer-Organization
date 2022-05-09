053e436da963c4c7691533647c96a368e5fca75b
lon5948 <a0988282303@gmail.com>
2022-05-10 01:01:46 +0800

    modify ALUCtrl.v

--- Single_Cycle_CPU/ALU_Ctrl.v
+++ Single_Cycle_CPU/ALU_Ctrl.v
@@ -6,22 +6,24 @@ module ALU_Ctrl(
     input       [2-1:0] ALUOp,
     output      [4-1:0] ALU_Ctrl_o
 );
+reg [3:0]out;
 wire [2:0] func3;
 assign func3 = instr[2:0];
+assign ALU_Ctrl_o = out;
 
 always@(*) begin
 	case(ALUOp)
-		2'b00: ALU_Ctrl_o <= 4'b0010;  // ld and sd
-		2'b01: ALU_Ctrl_o <= 4'b0110; // beq
+		2'b00: out <= 4'b0010;  // ld and sd
+		2'b01: out <= 4'b0110; // beq
 		2'b10: // R-type
 			case(instr)
-				4'b0000: ALU_Ctrl_o <= 4'b0010; // add 
-				4'b1000: ALU_Ctrl_o <= 4'b0110; // sub
-				4'b0111: ALU_Ctrl_o <= 4'b0000; // and
-				4'b0110: ALU_Ctrl_o <= 4'b0001; // or
-				4'b0010: ALU_Ctrl_o <= 4'b0111; // slt
+				4'b0000: out <= 4'b0010; // add 
+				4'b1000: out <= 4'b0110; // sub
+				4'b0111: out <= 4'b0000; // and
+				4'b0110: out <= 4'b0001; // or
+				4'b0010: out<= 4'b0111; // slt
 			endcase
-        2'b11: ALU_Ctrl_o <= 4'b0010; // addi
+        2'b11: out <= 4'b0010; // addi
 	endcase
 end
 
