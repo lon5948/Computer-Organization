beca87102a71f2224f90a18f754fbc6e7f4f5354
lon5948 <a0988282303@gmail.com>
2022-05-21 14:36:54 +0800

    forwarding unit

--- 5-Stage Pipeline Processor/Lab5/Lab5Code/ForwardingUnit.v
+++ 5-Stage Pipeline Processor/Lab5/Lab5Code/ForwardingUnit.v
@@ -9,6 +9,34 @@ module ForwardingUnit (
     output reg [2-1:0] ForwardA,
     output reg [2-1:0] ForwardB
 );
-/* Write your code HERE */
+
+always@(posedge clk_i) begin
+    
+    if(EXEMEM_RegWrite && EXEMEM_RD!=0 && IDEXE_RS1==EXEMEM_RD)
+    begin
+        ForwardA = 2'b10; 
+    end
+    else if(MEMWB_RegWrite && EXEMEM_RD!=0 && IDEXE_RS1==MEMWB_RD)
+    begin
+        ForwardA = 2'b01; 
+    end
+    else
+    begin
+        ForwardA = 2'b00; 
+    end
+
+    if(EXEMEM_RegWrite && EXEMEM_RD!=0 && IDEXE_RS2==EXEMEM_RD)
+    begin
+        ForwardB = 2'b10; 
+    end
+    else if(MEMWB_RegWrite && EXEMEM_RD!=0 && IDEXE_RS2==MEMWB_RD)
+    begin
+        ForwardB = 2'b01; 
+    end
+    else
+    begin
+        ForwardB = 2'b00; 
+    end
+end
 endmodule
 
